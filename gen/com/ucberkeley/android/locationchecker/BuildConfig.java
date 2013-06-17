193e7a62855d5b47fd43f8268b8aafddf885f9cc
bennyhuynh301 <bennyhuynh301@gmail.com>
2013-06-17 12:33:12 -0700

    first version

---
+++ gen/com/ucberkeley/android/locationchecker/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.ucberkeley.android.locationchecker;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
